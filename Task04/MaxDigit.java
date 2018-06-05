e1278d01102f7a9b363318ec7021e4c71762a680
Anna Shiman <33936122+Annshi@users.noreply.github.com>
2018-06-05 21:53:08 +0300

    Update MaxDigit.java

--- Task04/MaxDigit.java
+++ Task04/MaxDigit.java
@@ -1,15 +1,10 @@
-/**
- *
- * @version          1.3 02 June 2018
- * @author           Shiman Anna
- */
 public class MaxDigit {
     public static int defineMax(int numb) {
-        int next;
+        
         int max = getDigit(numb);//for max take last digit
 
         while (numb / 10 != 0) {
-            next = getDigit(numb = numb / 10);
+            int next = getDigit(numb = numb / 10);
             if (max < next) {
                 max = next;
             }
