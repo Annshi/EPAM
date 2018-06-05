77f1df17cddb82618a25edc69a12bba814c8ff69
Anna Shiman <33936122+Annshi@users.noreply.github.com>
2018-06-05 21:47:47 +0300

    Update Coin.java

--- Task04/Coin.java
+++ Task04/Coin.java
@@ -1,21 +1,34 @@
-/**
- *
- * @version          1.3 02 June 2018
- * @author           Shiman Anna
- */
-public class Coin {
-    public static final int VALUE_FOR_HEAD = 1;
-    public static final int VALUE_FOR_TAIL = 2;
-    public static String countSides(int tossAmount){
-        int heads = 0;
-        int tails = 0;
-        for(int i = 0; i < tossAmount ; i++){
-            if( VALUE_FOR_HEAD + (int)( Math.random() * VALUE_FOR_TAIL) == VALUE_FOR_HEAD ) {
-                heads++;
-            } else {
-                tails++;
+import java.util.Random;
+
+    public class Coin {
+
+        public static final int HEADS_VALUE = 0;
+        public static final int RANDOM_RANGE = 2;
+
+        public static String makeToss(int tossAmount) {
+            int heads = 0;
+            int tails = 0;
+
+            if (tossAmount < 1) {
+                return "There was no toss";
             }
+
+            Random side = new Random();
+            for (int i = 0; i < tossAmount; i++) {
+                if (side.nextInt(RANDOM_RANGE) == HEADS_VALUE) {
+                   heads = countHeads(heads);
+                } else tails =  countTails(tails);
+            }
+            return "heads = " + heads + " tails = " + tails;
         }
-        return "Amount of head: " + heads + ". Amount of tail: " +  tails;
+        
+            public static int countHeads(int heads ){
+            return ++heads; 
+        }
+
+             public static int countTails(int tails){
+                return ++tails; 
+        }
+
     }
-}
+
