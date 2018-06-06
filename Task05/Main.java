9c6d8cf7ddb8b266bb13cc0a56d28cf8ced1e1e8
Anna Shiman <33936122+Annshi@users.noreply.github.com>
2018-06-06 20:42:00 +0300

    Update Main.java

--- Task05/Main.java
+++ Task05/Main.java
@@ -1,24 +1,22 @@
+/*
+ * @(#)Main.java                  06.06 2018
+ *
+ * Copyright 1995-1999 Sun Microsystems, Inc.
+ * All rights reserved. 
+ *
+ * @author Shiman Anna
+ */
 public class Main {
     public static void main(String args[]){
 
-        int[] array = new int[3];
+        int[] array = new int[];
         ArrayInitializer.initRandom(array, -10, 10);
-        for (int v : array) {
-            System.out.printf(" "+v);
-        }
-        System.out.printf("Max number of massive is %d", MaxMin.findMax(array));
-        System.out.printf("\nMin number of massive is %d", MaxMin.findMin(array));
-        System.out.printf("\nIs massive ordered? %b", OrderedMassive.isOrdered(array));
+        System.out.printf("Max number of array is %d", MaxMin.findMax(array));
+        System.out.printf("\nMin number of array is %d", MaxMin.findMin(array));
+        System.out.printf("\nIs array ordered? %b", OrderedArray.isOrdered(array));
         System.out.printf("\nFirst local min = %d", LocalMin.defineFirstLocalMin(array));
         System.out.printf("\nMassive after reverse is ", Reverse.makeReverse(array));
-        for (int v : array) {
-            System.out.print(v);
-        }
         System.out.printf("\nAverage arithmetic is %f", ArithmGeom.calcAvgArith(array));
         System.out.printf("\nAverage geometric is %f", ArithmGeom.calcAvgGeom(array));
-        int tossAmount = 3;
-        System.out.printf("\n%s", Coin.makeToss(tossAmount));
-
-
     }
 }
