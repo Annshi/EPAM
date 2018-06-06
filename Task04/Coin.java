c761e09dae7ef781d3cbd3b597d0035403992db2
Anna Shiman <33936122+Annshi@users.noreply.github.com>
2018-06-06 10:25:19 +0300

    Update Coin.java

--- Task04/Coin.java
+++ Task04/Coin.java
@@ -2,12 +2,11 @@ import java.util.Random;
 
     public class Coin {
 
-        public static final int HEADS_VALUE = 0;
+        public static final int HEADS_AMOUNT = 0;
         public static final int RANDOM_RANGE = 2;
 
         public static String makeToss(int tossAmount) {
             int heads = 0;
-            int tails = 0;
 
             if (tossAmount < 1) {
                 return "There was no toss";
@@ -15,20 +14,11 @@ import java.util.Random;
 
             Random side = new Random();
             for (int i = 0; i < tossAmount; i++) {
-                if (side.nextInt(RANDOM_RANGE) == HEADS_VALUE) {
-                   heads = countHeads(heads);
-                } else tails =  countTails(tails);
+                if (side.nextInt(RANDOM_RANGE) == HEADS_AMOUNT) {
+                   heads++;
+                }
             }
-            return "heads = " + heads + " tails = " + tails;
+            return "heads = " + heads + " tails = " + (tossAmount - heads);
         }
-        
-            public static int countHeads(int heads ){
-            return ++heads; 
-        }
-
-             public static int countTails(int tails){
-                return ++tails; 
-        }
-
     }
 
