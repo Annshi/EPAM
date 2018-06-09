32d620b4eb5328db1209f4c2b8a970a79dd3de63
Anna Shiman <33936122+Annshi@users.noreply.github.com>
2018-06-09 23:06:08 +0300

    Update Reversion.java

--- Task05/Reversion.java
+++ Task05/Reversion.java
@@ -5,9 +5,9 @@ public class Reversion {
         }
 
         for (int i = 0; i < array.length / 2; i++) {//:2 as change places only at once
-            array[i] = array[i] ^ array[array.length - 1 - i];//lending by pairs to the middle of array
-            array[array.length - 1 - i] = array[i] ^ array[array.length - 1 - i];
-            array[i] = array[i] ^ array[array.length - 1 - i];
+            array[i] ^= array[array.length - 1 - i];//lending by pairs to the middle of array
+            array[array.length - 1 - i] ^= array[i];
+            array[i] ^= array[array.length - 1 - i];
         }
         return array;
     }
