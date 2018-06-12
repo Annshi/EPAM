70f40b78324fa89d5a827b56296c4c12a7f29cc3
Anna Shiman <33936122+Annshi@users.noreply.github.com>
2018-06-12 11:32:25 +0300

    Update Main.java

--- Task06/Main.java
+++ Task06/Main.java
@@ -32,8 +32,9 @@ public class Main {
             System.out.println(Arrays.toString(matrix[i]));
         }
         System.out.println("\nMatrix after transpose");
+        TransposedMatrix.transposeMatrix(matrix);
         for (int i = 0; i < matrix.length; i++) {
-            System.out.println(Arrays.toString(transposeMatrix.transposeMatrix(matrix)[i]));
+            System.out.println(Arrays.toString(matrix)[i]);
         }
 
 
