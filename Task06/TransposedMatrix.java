e94f4ef175ca20722aeaf05850f830e8f175abf8
Anna Shiman <33936122+Annshi@users.noreply.github.com>
2018-06-19 19:55:03 +0300

    Update TransposedMatrix.java

--- Task06/TransposedMatrix.java
+++ Task06/TransposedMatrix.java
@@ -1,5 +1,11 @@
 public class TransposedMatrix {
     public static void transposeMatrix(int[][] matrix) {
+        
+        for (int[] i : matrix) {
+            if (matrix.length != i.length) {
+                throw new IllegalArgumentException("Matrix isn't square");
+            }
+        }
 
         for (int i = 0; i < matrix.length; i++) {
             for (int j = i; j < matrix[i].length; j++) {
